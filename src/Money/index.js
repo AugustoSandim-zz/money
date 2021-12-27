a5e1a88ebeaf8582205a4d65ebd848382114b9c2
Augusto Sandim <augusto.sandim@rdstation.com>
2021-12-27 17:34:24 -0400

    Add private field

--- src/Money/index.js
+++ src/Money/index.js
@@ -1,14 +1,16 @@
 class Dollar {
+  #amount; // private instance variable
+
   constructor(amount) {
-    this.amount = amount;
+    this.#amount = amount;
   }
 
   times(multiplier) {
-    return new Dollar(this.amount * multiplier);
+    return new Dollar(this.#amount * multiplier);
   }
 
   equals(object) {
-    return object instanceof Dollar && this.amount === object.amount;
+    return object instanceof Dollar && this.#amount === object.#amount;
   }
 }
 
