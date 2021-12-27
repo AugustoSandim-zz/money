e069c24c7f5aab5501e6d2ad2f73331e34fe9282
Augusto Sandim <augustosandim@gmail.com>
2021-12-27 12:33:56 -0400

    Starting Cap2

--- src/Money/index.js
+++ src/Money/index.js
@@ -1,3 +1,10 @@
-class Dollar {}
+class Dollar {
+  constructor(amount) {
+    this.amount = amount;
+  }
+  times(multiplier) {
+    return new Dollar(this.amount * multiplier);
+  }
+}
 
-export default Dollar;
+module.exports = Dollar;
--- src/Money/index.test.js
+++ src/Money/index.test.js
@@ -12,24 +12,21 @@
  * 5. Refatore para remover duplicação
  */
 
-//  $5 + 10 CHF = $10 se a taxa é 2:1
-//  $5 * 2 = $10 | CHECKED
-//  Tornar “quantidade” privada
-//  Efeitos colaterais em Dollar?
-//  Arredondamento de dinheiro?
-class Dollar {
-  constructor(amount) {
-    this.amount = amount;
-  }
-  times(multiplier) {
-    this.amount *= multiplier;
-  }
-}
+/**  $5 + 10 CHF = $10 se a taxa é 2:1
+ *  $5 * 2 = $10 | CHECKED
+ *  Tornar “quantidade” privada
+ *  Efeitos colaterais em Dollar?
+ *  Arredondamento de dinheiro?
+ */
+
+const Dollar = require("./index");
 
 describe("Money", () => {
   it("Should be multiplication", () => {
-    const five = new Dollar(5);
-    five.times(2);
+    let five = new Dollar(5);
+    let product = five.times(2);
     expect(five.amount).toEqual(10);
+    product = five.times(3);
+    expect(product.amount).toEqual(15);
   });
 });
