d8204181b3b87ccd54c02eea9e19f96a63d88078
Augusto Sandim <augusto.sandim@rdstation.com>
2021-12-27 17:12:38 -0400

    Optmizing code

--- src/Money/index.test.js
+++ src/Money/index.test.js
@@ -28,10 +28,8 @@ const Dollar = require("./index");
 describe("Money", () => {
   it("Should be multiplication", () => {
     const five = new Dollar(5);
-    let product = five.times(2);
-    expect(new Dollar(10)).toEqual(product);
-    product = five.times(3);
-    expect(new Dollar(15)).toEqual(product);
+    expect(new Dollar(10)).toEqual(five.times(2));
+    expect(new Dollar(15)).toEqual(five.times(3));
   });
 
   it("Should be equality", () => {
